dcb66d644a4722d275a3c42c20079c1b2138bec8
Abdullah Mohammad Sakib <abdullahmohammadsakib@Abdullahs-MacBook-Pro.local>
2025-07-31 14:39:08 +0600

    Update

--- resources/views/frontend/spiritual-direction.blade.php
+++ resources/views/frontend/spiritual-direction.blade.php
@@ -108,7 +108,31 @@
 
             <!-- Right Card -->
             <div class="col-lg-4">
-                @include('frontend.comon.membership-card')
+                <style>
+                    .btn-orange {
+                        background-color: #ff9015 !important;
+                        border-color: #f4af1c !important;
+                        color: #fff !important;
+                    }
+                </style>
+                <div class="card hover-card shadow-sm border-0 rounded-4 overflow-hidden h-100">
+                    <div class="position-relative" style="height: 220px; overflow: hidden;">
+                        <img src="{{ asset('images/Imam-removebg-preview.png') }}" class="w-100 h-100"
+                            style="object-fit: contain" alt="Pastoral Care">
+                    </div>
+                    <div class="card-body p-4">
+                        <h5 class="card-title section-heading mb-3">AIMS Membership</h5>
+                        <p>Members of AIMS are bound by our Ethical Framework for the Counselling Professions and our
+                            Professional
+                            Conduct Procedure. You must read and agree to these as part of your application process. You
+                            should also
+                            read our membership policies.
+
+                        </p>
+                        <a href="{{ route('membership.index') }}" class="btn btn-orange mt-3">Read more...</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
